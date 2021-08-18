0ff4fd876aa3ee45b3c95b6cdb37d0e44353157e
Md Muftee <mahfuzul.muftee@tum.de>
2021-08-18 23:59:09 +0200

    ::construction: routing

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,18 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
+  Redirect
 } from "react-router-dom";
 import Home from "../src/pages/home";
+import About from "../src/pages/about";
 
 export default function Nav() {
   return (
-    <Router basename="https://mhmuftee.github.io">
+    <Router>
      <Switch>
+        <Route path="/" exact render={() => <Redirect to="/home" />} />
         <Route path="/home" component={Home} />
-        <Route path="*" component={Home} />
+        <Route path="/about" component={About} />
       </Switch>
     </Router>
   );
