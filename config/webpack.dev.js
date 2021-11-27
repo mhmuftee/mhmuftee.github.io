29a324236f30eafbbbc7ed251a12eab8de665580
muftee <mhmuftee@gmail.com>
2021-11-27 11:26:54 +0100

    :wrench: add host in webpack

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -11,7 +11,7 @@ module.exports = merge(common, {
     port: 9000,
     hot: true,
     open: true,
-    host: "0.0.0.0",
+    host: "local-ip",
     historyApiFallback: true,
     devMiddleware: {
       writeToDisk: true,
